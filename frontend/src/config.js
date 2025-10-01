242464c34885bcef62523f9962b005de38774ccd
sandeepdara-sd <sandeepdara44@gmail.com>
2025-10-01 23:52:56 +0530

    url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
-export const API_BASE_URL = "https://shopcart-rrmw.onrender.com/api";
+export const API_BASE_URL = "https://shopcart-69zh.onrender.com/api";
 
 
