89635970033f42d56f91594ebafe18bd30445dbe
sandeepdara-sd <sandeepdara44@gmail.com>
2025-10-01 23:12:10 +0530

    cors policy

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://sd-shopcart.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  // credentials: true,
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
