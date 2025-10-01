3fa860423a5db1268c4f47b0d0adac7c04c36583
sandeepdara-sd <sandeepdara44@gmail.com>
2025-10-01 23:49:22 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import wishlistRoutes from './routes/wishlist.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors({ origin: "*" }));
